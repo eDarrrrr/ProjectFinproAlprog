0bb6bbc6d0a33deb43a582374f124d5dd685f559
eDarrrrr <160409119+eDarrrrr@users.noreply.github.com>
2025-06-04 11:13:20 +0700

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 cd src
-g++ -o server.exe ServerNode.cpp -lws2_32
-g++ -o client.exe ClientNode.cpp -lws2_32
+g++ -o ..\server.exe ServerNode.cpp -lws2_32
+g++ -o ..\client.exe ClientNode.cpp -lws2_32
 echo Build selesai.
 pause

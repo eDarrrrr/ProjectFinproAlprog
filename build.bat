32b5735331996c33b97c6ac29b7a811d9bdbf8b8
eDarrrrr <160409119+eDarrrrr@users.noreply.github.com>
2025-06-04 11:18:27 +0700

    Update build.bat

--- build.bat
+++ build.bat
@@ -2,5 +2,10 @@
 cd src
 g++ -o ..\server.exe ServerNode.cpp -lws2_32
 g++ -o ..\client.exe ClientNode.cpp -lws2_32
+copy mahasiswa.csv ..\
+copy log_harian.bin ..\
+copy log_harian.json ..\
+copy log.txt ..\
+copy ClientNodeCamera.py ..\
 echo Build selesai.
 pause
